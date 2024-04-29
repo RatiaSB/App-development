32a5134b8745d3aa69bd5714d97a1c964051c2d6
RatiaSB <230724477@mycput.ac.za>
2024-04-29 08:15:23 +0200

    added prac 7 to be changed to 9

---
+++ ADP262/Practical 7/src/za/ac/cput/Prac7.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.cput;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.PopupMenu;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author user10
+ */
+public class Prac7 extends JFrame {
+//Create Instances of JPanels
+    private JPanel panelTop = new JPanel();
+    private JPanel panelCenter = new JPanel();
+    private JPanel panelBottom = new JPanel();
+//Create Instances of Labels
+    private JLabel lblDepartment = new JLabel("Department:");
+    private JLabel lblFirstName = new JLabel("First Name:");
+    private JLabel lblLastName = new JLabel("Last Name:");
+    private JLabel lblEmploymentType = new JLabel("Employment Type:");
+
+//Create Instances of textfields 
+    private JTextField txtFirstName = new JTextField();
+    private JTextField txtLastName = new JTextField();
+//Create Instance of ComboBox and add items in list
+    String[] departments = {"select","Graphic Design","Civil Engineering","Information Technology"}; 
+    private JComboBox cboDepartment = new JComboBox(departments);
+    
+//Create Instances of the radiobuttons and group them
+    private JRadioButton radPermanent = new JRadioButton("Permanent");
+    private JRadioButton radTemporary = new JRadioButton("Temporary");
+    
+//Create Instances of the buttons  
+    private JButton btnAddToTable = new JButton("Add to Table");
+    private JButton btnExit = new JButton("Exit");
+    
+//Create Instance of table
+    Object[] columns = {"Department","FirstName","LastName","EmployementType"};
+    DefaultTableModel model = new DefaultTableModel();
+    private JTable table = new JTable(model);
+    private JScrollPane scrollPane = new JScrollPane(table);
+    
+    
+    public Prac7() {
+        super("University Staff Application");
+        //Construct JPanels
+        panelTop.setLayout(new GridLayout(6,2,5,5));
+        //panelBottom.setLayout(new GridLayout(1, 1, 5, 5));
+        
+        
+        panelTop.add(lblDepartment);
+        panelTop.add(cboDepartment);
+        panelTop.add(lblFirstName);
+        panelTop.add(txtFirstName);
+        panelTop.add(lblLastName);
+        panelTop.add(txtLastName);
+        panelTop.add(lblEmploymentType);
+        panelTop.add(radPermanent);
+        panelTop.add(radTemporary);
+        
+        
+        
+        
+        this.setLayout(new BorderLayout());
+        this.add(panelTop);
+        //this.add(panelBottom, BorderLayout.WEST);
+    }
+   
+    
+}
---
+++ ADP262/Practical 7/src/za/ac/cput/runPrac7.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.cput;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author user10
+ */
+public class runPrac7 {
+
+    public static void main(String[] args) {
+        Prac7 objGui = new Prac7();
+        
+        objGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        objGui.setSize(700, 500);
+        objGui.setVisible(true);
+        objGui.setResizable(true);
+    }
+
+}
