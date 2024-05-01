bf8a4594e0235ba92ab42b10bf113c7afb5eb54b
RatiaSB <230724477@mycput.ac.za>
2024-05-01 21:19:22 +0200

    Tested BufferedWriter and FileWriter classes

--- ADP262/SimpleWritetofile/src/Main.java
+++ ADP262/SimpleWritetofile/src/Main.java
@@ -7,16 +7,16 @@ public class Main {
         String heading = String.format("%-15s%-15s%-15s%-15s\n", "Student Name", "Student Number",
                 "Qualification", "Date of Birth");
         String student1 = String.format("%-15s%-15s%-15s%-15s\n", "John Smith", "24636578", "BCom", "19980328");
+        String student2 = String.format("%-15s%-15s%-15s%-15s\n", "James Smith", "24636579", "Masters", "19970429");
         try {
 
-            FileWriter fw = new FileWriter("Student.txt");
-            //BufferedWriter bw = new BufferedWriter(fw);
-            //bw.write(heading);
-            //bw.write(student1);
-            //bw.close();
-            fw.write(heading);
-            fw.write(student1);
-            fw.close();
+            FileWriter fw = new FileWriter("Student1.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(heading);
+            bw.write(student2);
+            bw.write(student1);
+            bw.close();
+
             System.out.println("opened and wrote to file successfully");
 
         } catch (IOException e) {
