a84122ca5fe1ef76cf81eace49444c559364a08d
RatiaSB <230724477@mycput.ac.za>
2024-05-05 12:50:40 +0200

    practice code

---
+++ ADP262/Error Handling/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            //block of code
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
