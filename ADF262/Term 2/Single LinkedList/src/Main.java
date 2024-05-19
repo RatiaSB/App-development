80eb71f186ebbf520ba7b2aad625f5ee9243a9f0
RatiaSB <230724477@mycput.ac.za>
2024-05-19 14:14:04 +0200

    inner class

--- ADF262/Term 2/Single LinkedList/src/Main.java
+++ ADF262/Term 2/Single LinkedList/src/Main.java
@@ -1,9 +1,28 @@
 
 
 public class Main {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public static void traverseAndprint(Node head) {
+            Node currentNode = head;
+            while (currentNode != null) {
+                System.out.print(currentNode.data + " -> ");
+                currentNode = currentNode.next;
+            }
+            System.out.println("null");
+        }
+    }
+
     public static void main(String[] args) {
-//Pass Elements(data) of the nodes are 3, 2, 6 ,9
-        Node firstNode = new Node(3);
+//Pass Elements(data) of the nodes are 8, 2, 6 ,9
+        Node firstNode = new Node(8);
         Node secNode = new Node(2);
         Node thirdNode = new Node(6);
         Node forthNode = new Node(9);
@@ -12,12 +31,8 @@ public class Main {
         secNode.next = thirdNode;
         thirdNode.next = forthNode;
 
-        Node currentNode = firstNode;
+        Node.traverseAndprint(firstNode);
 
-        while (currentNode != null){
-            System.out.print(currentNode.data + " -> ");
-            currentNode = currentNode.next;
-        }
-        System.out.println("null");
+
     }
 }
